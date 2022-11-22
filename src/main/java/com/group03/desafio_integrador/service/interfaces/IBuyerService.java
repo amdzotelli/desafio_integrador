9a8a167cba4eca579cc6bba78fad05ae75c1b2c2
Amanda Zotelli <amanda.zotelli@mercadolivre.com>
2022-11-21 22:25:30 -0300

    :rocker: feat: create buyer service interface with get all method

--- src/main/java/com/group03/desafio_integrador/service/interfaces/IBuyerService.java
+++ src/main/java/com/group03/desafio_integrador/service/interfaces/IBuyerService.java
@@ -2,6 +2,8 @@ package com.group03.desafio_integrador.service.interfaces;
 
 import com.group03.desafio_integrador.entities.Buyer;
 
+import java.util.List;
+
 public interface IBuyerService {
     /**
      * Método responsável por retornar o comprador conforme Id informado.
@@ -10,4 +12,7 @@ public interface IBuyerService {
      * @return Retorna uma entidade do tipo Buyer.
      */
     Buyer getById(Long id);
+
+    List<Buyer> getAll();
+
 }
